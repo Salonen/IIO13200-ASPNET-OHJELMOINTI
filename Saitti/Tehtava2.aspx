a99e602ffc6f702b4a61e94073c160d8a118dacc
h8482 <h8482@ICT-422-WS-06>
2016-09-19 13:14:36 +0300

    Lotto toimii.

---
+++ Saitti/Tehtava2.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Tehtava2.aspx.cs" Inherits="Tehtava2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Lotto</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:Button ID="Button1" runat="server" Text="Arvo numerot" OnClick="Button1_Click" />
+        <p>
+            <asp:TextBox ID="TextBox1" runat="server" Height="229px" Width="169px"></asp:TextBox>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Saitti/Tehtava2.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Web;
+using System.Data; // for datatable yms
+using System.Configuration;
+
+public partial class Tehtava2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            string rivi = "";
+
+            Random rnd = new Random();
+
+            rivi = JAMK.ICT.Data2.Lotto.Luvut(7,39,rnd); // KIRJOITIN UUDESTAAN JA ... yms. ... // VIEW BROWSER
+
+            TextBox1.Text = rivi;
+
+            //rivi = l.Luvut(7, 39, rnd);
+
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+    }
+}
+
