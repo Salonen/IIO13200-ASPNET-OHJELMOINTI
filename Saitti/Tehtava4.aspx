b4e2573e9d2f3be599521324b8c1d58d47b4dc88
h8482 <h8482@MEDI-426-WS-03>
2016-10-05 11:06:40 +0300

    Toiselllla tavalla toimii.

---
+++ Saitti/Tehtava4.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Tehtava4.aspx.cs" Inherits="Tehtava4" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:TextBox ID="TextBox1" runat="server" Height="31px" Width="142px"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        <asp:Label ID="lblUserName" runat="server" />
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Saitti/Tehtava4.aspx.cs
@@ -0,0 +1,43 @@
+﻿using JAMK.IT;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Tehtava4 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        DataTable koo = new DataTable();
+        DataTable joo = new DataTable();
+
+        koo = DBDemoxOy.GetDataSimple();
+        GridView1.DataSource = koo;
+        GridView1.DataBind();
+
+        /*        DataTable dt = new DataTable();
+                dt = (DataTable)Session["userdata"];*/
+        lblUserName.Text = koo.Rows.ToString();//your cloumn name;
+        //lblUserName.Text = dt.Rows[0]["UserName"].ToString();//your cloumn name;
+        joo = DBDemoxOy.GetDataReal();
+
+        GridView1.DataSource = joo;
+        GridView1.DataBind();
+        
+        lblUserName.Text = joo.Rows.ToString();//your cloumn name;
+
+        /*TextBox1.Text = koo;
+        DataList1 = koo;
+        // Instruct the DataGrid to bind to the DataSet, with the 
+        // ParentTable as the topmost DataTable.
+        GridView1.SetDataBinding(dataSet, "ParentTable");*/
+    }
+}
