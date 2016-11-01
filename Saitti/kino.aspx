3bd4ecc838234af0079817c96be7065a9b6ebad1
h8482 <h8482@ICT-422-WS-04>
2016-11-01 16:18:11 +0200

    Kino toimii jonkun  verran.

--- Saitti/kino.aspx
+++ Saitti/kino.aspx
@@ -4,21 +4,19 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title>Työntekijät</title>
+    <title>Elokuvat</title>
 </head>
 <body>
     <form id="form1" runat="server">
     <div>
-        <asp:Button ID="btnHae" runat="server" Text="Hae työntekijät" OnClick="btnHae_Click" />
+        <asp:Button ID="btnHae" runat="server" Text="Hae elokuvat" OnClick="btnHae_Click" />
         <div id="esitys">
-             <h2>Työntekijämme</h2>
+             <h2>Elokuvat</h2>
             <asp:Repeater ID="Repeater1" runat="server" >
         <ItemTemplate>
             <asp:Image ID="Image1" Width="150px" ImageUrl='<%# Container.DataItem  %>' runat="server" /> 
         </ItemTemplate>
     </asp:Repeater>
-            <asp:GridView 
-                ID="gvData" runat="server"></asp:GridView>
         </div>
         <div id="footer">
             <asp:Label ID="lblMessages" 
@@ -26,8 +24,6 @@
                 Text="..."></asp:Label>
         </div>
     </div>
-        <asp:TextBox ID="TextBox1" runat="server" Height="100px" Width="1435px"></asp:TextBox>
-        <asp:ListBox ID="ListBox1" runat="server" Height="217px" Width="353px"></asp:ListBox>
         <asp:TextBox ID="TextBox2" runat="server" Height="246px" Width="422px" TextMode="MultiLine"></asp:TextBox>
     </form>
 </body>
--- Saitti/kino.aspx.cs
+++ Saitti/kino.aspx.cs
@@ -25,53 +25,39 @@ using System.Xml;
 
 public partial class Tyontekijat : System.Web.UI.Page
 {
-    string xmlfilu;
-    protected void Page_Load(object sender, EventArgs e)
-    {
-        //haetaan web.configista xml-tiedoston nimi
-        xmlfilu = ConfigurationManager.AppSettings["kino"];
-        lblMessages.Text = xmlfilu;
-    }
-
+    
     protected void btnHae_Click(object sender, EventArgs e)
     {
-        //haetaan XML-data DataView-olioon, joka kytketään GridViewhen
-        DataSet ds = new DataSet();
-        DataTable dt = new DataTable();
-        DataView dv = new DataView();
 
-        /*XmlTextReader reader = new XmlTextReader("http://www.w3.org/2001/XMLSchema");
-        int n = 0;
-        while (reader.Read())
+        try
         {
-            // Do some work here on the data.
-            TextBox1.Text = reader.ToString();
-        }*/
+
+        
 
         String URLString = "http://www.finnkino.fi/xml/TheatreAreas/";
         XmlTextReader reader = new XmlTextReader(URLString);
 
         XmlDocument doc = new XmlDocument();
-        doc.Load(reader/*@"G:\C#2\Viinit1.xml"*/);
+        doc.Load(reader);
 
         XmlNodeList elemList = doc.GetElementsByTagName("Name");
         XmlNodeList elemListB = doc.GetElementsByTagName("ID");
 
-        //TextBox2.Text = elemList[0].InnerText;
+   
 
         String URLString2 = "http://www.finnkino.fi/xml/Schedule/";
         XmlTextReader reader2 = new XmlTextReader(URLString2);
 
         XmlDocument doc2 = new XmlDocument();
-        doc2.Load(reader2/*@"G:\C#2\Viinit1.xml"*/);
+        doc2.Load(reader2);
 
-        //string g = elemList[0].
+       
 
         int pois = 1;
         int i = 0;
         for (; i < elemList.Count && pois == 1; i++)
         {
-            if (elemList[i].InnerText == "Espoo: Sello")
+            if (elemList[i].InnerText == "Tampere: Cine Atlas")
             {
                 pois = 0;
                 i--;
@@ -85,7 +71,7 @@ public partial class Tyontekijat : System.Web.UI.Page
         XmlNodeList elemList2B = doc2.GetElementsByTagName("TheatreID");
         XmlNodeList elemList2C = doc2.GetElementsByTagName("EventMicroImagePortrait");
 
-        //string g=elemList2
+        
         List<string> url = new List<string>();
 
 
@@ -98,61 +84,16 @@ public partial class Tyontekijat : System.Web.UI.Page
             }
         }
 
-        /*for (int i = 0; i < elemList.Count; i++)
-        {
-            TextBox2.Text += elemList[i].InnerText;
-
-        }*/
+        
 
         Repeater1.DataSource = url;
         Repeater1.DataBind();
 
-        string x = "";
-
-        while (reader.Read())
-        {
-            switch (reader.NodeType)
-            {
-                case XmlNodeType.Element: // The node is an element.
-                    ListBox1.Items.Add(string.Format("<" + reader.Name));//.ToString();
-
-                    while (reader.MoveToNextAttribute()) // Read the attributes.
-                        ListBox1.Items.Add((" " + reader.Name + "='" + reader.Value + "'"));//.ToString();
-                    ListBox1.Items.Add(string.Format(">"));//.ToString();
-                    ListBox1.Items.Add(string.Format(">"));//.ToString();
-                    break;
-                case XmlNodeType.Text: //Display the text in each element.
-                    ListBox1.Items.Add(string.Format(reader.Value));//.ToString();
-                    break;
-                case XmlNodeType.EndElement: //Display the end of the element.
-                    ListBox1.Items.Add(string.Format("</" + reader.Name));// ToString();
-                    ListBox1.Items.Add(string.Format(">"));//.ToString();
-                    break;
-            }
         }
-        TextBox1.Text = x.ToString(); // TOIMII!!!
-
-        /*dt = x;
-        dv = dt.DefaultView;
-        gvData.DataSource = dv;
-        gvData.DataBind();*/
-
-        //ListBox1.Items.Add(x);
-
-
-
-        /*ds.ReadXml(Server.MapPath("http://www.w3.org/2001/XMLSchema"));//huom MapPath muuttaa viittauksen websaitille
-        dt = ds.Tables[0];
-        dv = dt.DefaultView;
-        gvData.DataSource  = dv;
-        gvData.DataBind();
-
-        for (int n = 0; n < 3; n++)
+        catch (Exception)
         {
-            dt = ds.Tables[n];
-            dv = dt.DefaultView;
-            gvData.DataSource = dv;
-            gvData.DataBind();
-        }*/
+
+            throw;
+        }
     }
 }
